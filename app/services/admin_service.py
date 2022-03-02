302250a0ef23f3ddf347b6d805f1cb6c57a0dd13
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-02 12:21:12 -0300

    feat: validation request for create an new admin

--- app/services/admin_service.py
+++ app/services/admin_service.py
@@ -4,8 +4,9 @@ from werkzeug.exceptions import BadRequest
 def validate_admin(request_data: dict):
     valid_keys = set(getenv('ADM_KEYS').split(","))
     wrong_keys = set(request_data.keys()) - valid_keys
+    wrong_values_type = [ value for value in request_data.values() if type(value) != str ]
 
-    if len(request_data) < 2:
+    if len(request_data) < 3:
         missing_keys = valid_keys - set(request_data.keys())
         error_description = {"missing keys": list(missing_keys)}
 
@@ -21,3 +22,10 @@ def validate_admin(request_data: dict):
             error_description = {"wrong key": list(wrong_keys)[0]}
 
         raise BadRequest(description=error_description)
+
+    if wrong_values_type:
+        raise BadRequest(
+            description={"error_message": "All field values must be a string type"}
+        )
+
+    return request_data
