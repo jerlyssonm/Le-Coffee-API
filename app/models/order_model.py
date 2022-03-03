7383a898cc11998f8c6f797ae7c000bd905aa186
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-03-03 14:05:59 -0300

    feat: Order model created

--- app/models/order_model.py
+++ app/models/order_model.py
@@ -1,20 +1,23 @@
+from xmlrpc.client import DateTime
 from app.configs.database import db
 from datetime import datetime
-from sqlalchemy import Column, Integer, Boolean, ForeignKey
+from sqlalchemy import Column, Integer, Boolean, ForeignKey, DateTime
 from dataclasses import dataclass
+from sqlalchemy.orm import relationship
 
 @dataclass
 class OrderModel(db.Model):
     id: int
-    status: Boolean
-    date: datetime
+    status: bool
+    date: datetime.now()
     user_id: int
 
-    __tablename__ = "order"
+    __tablename__ = "orders"
 
-    id = Column(Integer, primary_key=True)
-    status = Column(Boolean)
-    date = Column(datetime)
+    order_id = Column(Integer, primary_key=True)
+    status = Column(Boolean, nullable=False)
+    date = Column(DateTime(), nullable=False, default=datetime.now())
     user_id = Column(Integer, ForeignKey("users.id"))
 
-    estados = db.relationship("EstadoModel", backref="regiao")
+    user = relationship("UserModel", back_populates="order")
+    product = relationship("Product", secondary="product_order")
