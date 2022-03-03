cb3255cd8d4d949625c110196142693c978f29ef
Roberto Rocha <rsantos.rocha@gmail.com>
2022-03-03 00:44:18 -0300

    feat: addresses model created

---
+++ app/models/address_model.py
@@ -0,0 +1,25 @@
+from app.configs.database import db 
+from dataclasses import dataclass
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+@dataclass
+class AddressModel(db.Model):
+    address_id: int
+    street: str
+    number: str
+    city: str
+    state: str
+    country: str
+    cep: str
+    user_id: int
+
+    __tablename__ = "addresses"
+
+    address_id = Column(Integer, primary_key = True)
+    street = Column(String, nullable=False)
+    number = Column(String, nullable=False)
+    city = Column(String, nullable=False)
+    country = Column(String, nullable=False, default="Brasil")
+    cep = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
+
