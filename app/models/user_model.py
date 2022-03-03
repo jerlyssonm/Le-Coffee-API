17b75a9aa64b81fc16c01680e7205db623203ad0
Roberto Rocha <rsantos.rocha@gmail.com>
2022-03-03 00:03:06 -0300

    feat: user model was created

---
+++ app/models/user_model.py
@@ -0,0 +1,30 @@
+from enum import unique
+from app.configs.database import db 
+from dataclasses import dataclass
+from werkzeug.security import generate_password_hash, check_password_hash
+
+@dataclass
+class UserModel(db.Model):
+    user_id: int
+    name: str
+    email: str
+    password: str
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password_hash = db.Column(db.String, nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be acessed!")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def verify_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
+    
