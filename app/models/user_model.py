c128d155a5451eb5d58ae67ab8880220849b6e48
Roberto Rocha <rsantos.rocha@gmail.com>
2022-03-03 08:44:33 -0300

    fix: user model with hash

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,12 +1,12 @@
 from app.configs.database import db 
 from dataclasses import dataclass
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @dataclass
 class UserModel(db.Model):
     user_id: int
     name: str
     email: str
-    password: str
 
     __tablename__ = "users"
 
@@ -15,4 +15,13 @@ class UserModel(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=False)
 
-    
+    @property
+    def password(self):
+        raise AttributeError("Password cannot be acessed!")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def verify_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
