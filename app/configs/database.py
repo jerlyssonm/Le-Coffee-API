e280efc34a5107945d15b24a0e569569392ee084
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-03 20:40:22 -0300

    refactor: imported all models

--- app/configs/database.py
+++ app/configs/database.py
@@ -9,10 +9,10 @@ def init_app(app: Flask):
 
     from app.models.admin_model import AdminModel
     from app.models.user_model import UserModel
-    # from app.models.region_model import RegionModel
-    # from app.models.product_model import ProductModel
-    # from app.models.order_model import OrderModel
-    # from app.models.product_order_model import products_orders
+    from app.models.region_model import RegionModel
+    from app.models.product_model import ProductModel
+    from app.models.order_model import OrderModel
+    from app.models.product_order_model import products_orders
     from app.models.address_model import AddressModel
-    # from app.models.feedback_model import FeedbackModel
+    from app.models.feedback_model import FeedbackModel
 
