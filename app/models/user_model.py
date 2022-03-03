60ba88b67867c4e8f6b920e2efaf068e3d31e1c5
Roberto Rocha <rsantos.rocha@gmail.com>
2022-03-03 00:20:30 -0300

    feat: user model updated

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,6 @@
 from enum import unique
 from app.configs.database import db 
 from dataclasses import dataclass
-from werkzeug.security import generate_password_hash, check_password_hash
 
 @dataclass
 class UserModel(db.Model):
@@ -17,14 +16,5 @@ class UserModel(db.Model):
     email = db.Column(db.String, nullable=False, unique=True)
     password_hash = db.Column(db.String, nullable=False)
 
-    @property
-    def password(self):
-        raise AttributeError("Password cannot be acessed!")
-
-    @password.setter
-    def password(self, password_to_hash):
-        self.password_hash = generate_password_hash(password_to_hash)
-
-    def verify_password(self, password_to_compare):
-        return check_password_hash(self.password_hash, password_to_compare)
+    
     
