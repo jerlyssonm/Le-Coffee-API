d5d9125d9d434df5b4137230583660ecc4478e4b
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-04 01:12:07 -0300

    refactor: controller functions names

--- app/routes/admin_blueprint.py
+++ app/routes/admin_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app.controllers import admin_controller
 bp_admin = Blueprint('admin', __name__, url_prefix='/admin')
 
-bp_admin.post('/register')(admin_controller.create_admin)
-bp_admin.post('/login')(admin_controller.login_admin)
-bp_admin.patch('')(admin_controller.update_admin)
-bp_admin.delete('')(admin_controller.delete_admin)
+bp_admin.post('/register')(admin_controller.signup)
+bp_admin.post('/login')(admin_controller.signin)
+# bp_admin.patch('')(admin_controller.update_admin)
+# bp_admin.delete('')(admin_controller.delete_admin)
