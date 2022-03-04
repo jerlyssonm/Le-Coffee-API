02a5b502bd76f82598cedf2567486b41f64ff017
PauloHenriqueMagno <paulohm2309@gmail.com>
2022-03-04 15:27:23 -0300

    feat: add region blueprint

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,9 +3,10 @@ from flask import Flask
 from app.routes.admin_blueprint import bp_admin
 from app.routes.product_blueprint import bp_products
 from app.routes.user_blueprint import bp_user
-
+from app.routes.region_blueprint import bp_region
 
 def init_app(app: Flask):
     app.register_blueprint(bp_admin)
     app.register_blueprint(bp_user)
     app.register_blueprint(bp_products)
+    app.register_blueprint(bp_region)
