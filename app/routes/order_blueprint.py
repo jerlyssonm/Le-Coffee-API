56f8f7db68ebf360626c3b62b4b3dcf8ea49f83c
Kelvin Crepaldi <kelvin.creps@hotmail.com>
2022-03-04 14:18:55 -0300

    feat: order blueprint created

--- app/routes/order_blueprint.py
+++ app/routes/order_blueprint.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
 from app.controllers import order_controller
+
 bp_order = Blueprint('order', __name__, url_prefix='/orders')
 
-bp_admin.post('/register')(admin_controller.signup)
-bp_admin.post('/login')(admin_controller.signin)
-# bp_admin.patch('')(admin_controller.update_admin)
-# bp_admin.delete('')(admin_controller.delete_admin)
-
+bp_order.post('')(order_controller.create_order)
+bp_order.get('')(order_controller.get_orders_list)
+bp_order.get('/<int:order_id>')(order_controller.get_order_by_id)
