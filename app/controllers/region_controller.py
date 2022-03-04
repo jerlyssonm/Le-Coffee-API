05ad635f514814938e6f3e7c84bb37f3c5ef5151
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-04 16:01:55 -0300

    fix: region controller request body capturing

--- app/controllers/region_controller.py
+++ app/controllers/region_controller.py
@@ -15,7 +15,7 @@ def get_regions():
 
 @auth.login_required
 def create_region():
-  region_data = request.json
+  region_data = request.get_json()
 
   try:
     session = current_app.db.session
@@ -56,7 +56,7 @@ def delete_region(region_id: int):
 
 @auth.login_required
 def modify_region(region_id: int):
-  region_data = dict(request.json)
+  region_data = request.get_json()
 
   try:
     session = current_app.db.session
