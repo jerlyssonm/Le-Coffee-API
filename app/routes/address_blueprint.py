5d0475862972df4f1610950062d52b7218ae7169
Roberto Rocha <rsantos.rocha@gmail.com>
2022-03-04 14:40:06 -0300

    fix: parameter address_id added to get, patch and delete routes

--- app/routes/address_blueprint.py
+++ app/routes/address_blueprint.py
@@ -4,5 +4,6 @@ bp_address = Blueprint('address', __name__, url_prefix='/address')
 
 bp_address.post('')(address_controller.create_record)
 bp_address.get('')(address_controller.get_records)
-bp_address.patch('')(address_controller.update_record)
-bp_address.delete('')(address_controller.delete_record)
+bp_address.get('/<int:address_id>')(address_controller.get_record_by_id)
+bp_address.patch('/<int:address_id>')(address_controller.update_record)
+bp_address.delete('/<int:address_id>')(address_controller.delete_record)
