2555c27c322021a483735fb7d3f4d48fc817b744
jerlyssonm <jerlyssonm@gmail.com>
2022-03-04 14:14:43 -0300

    fix: correct route

--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -6,5 +6,5 @@ bp_user = Blueprint("users",__name__, url_prefix="/user")
 
 bp_user.post("/register")(user_controller.signup)
 bp_user.post("/login")(user_controller.signin)
-bp_user.get("")(user_controller.get_product_all)
-bp_user.get("/<int:id>")(user_controller.get_product_by_id)
+bp_user.get("")(user_controller.get_user_all)
+bp_user.get("/<int:id>")(user_controller.get_user_by_email)
