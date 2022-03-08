92862a876db2abe9d94d8bbe7888df7d17b17f17
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-08 15:11:05 -0300

    refactor: function to get all orders requires admin auth

--- app/routes/order_blueprint.py
+++ app/routes/order_blueprint.py
@@ -5,6 +5,7 @@ bp_order = Blueprint('order', __name__, url_prefix='/orders')
 
 bp_order.post('')(order_controller.create_order)
 bp_order.get('')(order_controller.get_all_orders)
+bp_order.get('')(order_controller.get_orders_by_user)
 bp_order.get('/<int:order_id>')(order_controller.get_order_by_id)
 bp_order.get('/<int:order_id>/products')(order_controller.get_order_products)
 bp_order.delete('/<int:order_id>')(order_controller.delete_order_by_id)
