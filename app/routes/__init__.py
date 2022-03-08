43665abc14c03c5a1670fb1f000f1a7428334096
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-08 19:46:57 -0300

    fix: feedbacks

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -6,6 +6,7 @@ from app.routes.user_blueprint import bp_user
 from app.routes.region_blueprint import bp_region
 from app.routes.address_blueprint import bp_address
 from app.routes.order_blueprint import bp_order
+from app.routes.feedback_blueprint import bp_feedback
 
 def init_app(app: Flask):
     app.register_blueprint(bp_admin)
@@ -14,3 +15,4 @@ def init_app(app: Flask):
     app.register_blueprint(bp_region)
     app.register_blueprint(bp_address) 
     app.register_blueprint(bp_order)
+    app.register_blueprint(bp_feedback)
--- app/routes/feedback_blueprint.py
+++ app/routes/feedback_blueprint.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 from app.controllers import feedback_controller
-bp_feedback = Blueprint('feedback', __name__, url_prefix='/feedback')
+bp_feedback = Blueprint('feedback', __name__, url_prefix='/feedbacks')
 
-bp_feedback.post('')(feedback_controller.create_record)
-bp_feedback.get('')(feedback_controller.get_records)
-bp_feedback.get('/<int:feedback_id>')(feedback_controller.get_record_by_id)
-bp_feedback.patch('/<int:feedback_id>')(feedback_controller.update_record)
-bp_feedback.delete('/<int:feedback_id>')(feedback_controller.delete_record)
+bp_feedback.post('/<int:product_id>')(feedback_controller.create_feedback)
+bp_feedback.get('')(feedback_controller.get_all_feedbacks)
+bp_feedback.get('/<int:feedback_id>')(feedback_controller.get_feedback_by_id)
+bp_feedback.patch('/<int:feedback_id>')(feedback_controller.update_feedback)
+bp_feedback.delete('/<int:feedback_id>')(feedback_controller.delete_feedback)
