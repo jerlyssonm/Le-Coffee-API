e47e03d6c637df181c65c26ffaa7c8d233bd4d55
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-07 22:30:00 -0300

    fix: initialized address routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,9 +4,11 @@ from app.routes.admin_blueprint import bp_admin
 from app.routes.product_blueprint import bp_products
 from app.routes.user_blueprint import bp_user
 from app.routes.region_blueprint import bp_region
+from app.routes.address_blueprint import bp_address
 
 def init_app(app: Flask):
     app.register_blueprint(bp_admin)
     app.register_blueprint(bp_user)
     app.register_blueprint(bp_products)
     app.register_blueprint(bp_region)
+    app.register_blueprint(bp_address)
