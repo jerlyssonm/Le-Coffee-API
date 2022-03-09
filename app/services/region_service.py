b4e180f3823d56ed154ff09f33818d52c1d644ff
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2022-03-09 11:27:13 -0300

    refactor: coordenates error handling

--- app/services/region_service.py
+++ app/services/region_service.py
@@ -41,28 +41,26 @@ def check_region_data(data: dict):
     match_response_latitude = re.fullmatch(match_rule_latitude, latitude)
 
     if match_response_latitude is None:
-      raise ValueError({
-        "msg": "latitude is invalid",
-        "valid_latitude": {
-          "max_value": "+90.000",
-          "min_value": "-90.00"
-        },
-        "invalid_latitude": latitude
-      })
+        raise BadRequest(
+            description={
+                "error_message": "latitude is invalid",
+                "valid_latitude": {"max_value": "+90.000", "min_value": "-90.00"},
+                "invalid_latitude": latitude,
+            }
+        )
 
     longitude: str = data["longitude"]
     match_rule_longitude = r"^(\+|-)?(?:180(?:(?:\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\.[0-9]{1,6})?))$"
     match_response_longitude = re.fullmatch(match_rule_longitude, longitude)
 
     if match_response_longitude is None:
-        raise ValueError({
-        "msg": "longitude is invalid",
-        "valid_longitude": {
-          "max_value": "+180.000",
-          "min_value": "-180.000"
-        },
-        "invalid_longitude": longitude
-      })
+        raise BadRequest(
+            description={
+                "error_message": "longitude is invalid",
+                "valid_longitude": {"max_value": "+180.000", "min_value": "-180.000"},
+                "invalid_longitude": longitude,
+            }
+        )
 
     if "name" in data.keys():
         name: str = data["name"]
